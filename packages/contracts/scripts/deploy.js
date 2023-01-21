be69778d11b296f948e2b8db2bd237f9a6f24416
maguro <maguro.abs@gmail.com>
2023-01-21 21:51:31 +0900

    feat: web app

--- packages/contracts/scripts/deploy.js
+++ packages/contracts/scripts/deploy.js
@@ -12,19 +12,6 @@ async function main() {
   await domainContract.deployed();
 
   console.log("Contract deployed to:", domainContract.address);
-
-  let tx = await domainContract.register("maguro", {
-    value: hre.ethers.utils.parseEther("0.1"),
-  });
-
-  await tx.wait();
-  console.log("Set record for maguro.fish");
-
-  const address = await domainContract.getAddress("maguro");
-  console.log("Owner of domain maguro:", address);
-
-  const balance = await hre.ethers.provider.getBalance(domainContract.address);
-  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- packages/contracts/scripts/run.js
+++ packages/contracts/scripts/run.js
@@ -1,21 +1,52 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const [owner, hacker] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy("fish");
   await domainContract.deployed();
 
   console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract owner:", owner.address);
 
-  let txn = await domainContract.register("maguro", {
-    value: hre.ethers.utils.parseEther("0.01"),
+  let tx = await domainContract.register("maguro", {
+    value: hre.ethers.utils.parseEther("1234"),
   });
-  await txn.wait();
+  await tx.wait();
 
   const domainOwner = await domainContract.getAddress("maguro");
   console.log("Owner of domain maguro:", domainOwner);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  try {
+    tx = await domainContract.connect(hacker).withdraw();
+    await tx.wait();
+  } catch (error) {
+    console.log("Could not rob contract");
+  }
+
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+  console.log(
+    "Balance of owner before withdrawal:",
+    hre.ethers.utils.formatEther(ownerBalance)
+  );
+
+  tx = await domainContract.connect(owner).withdraw();
+  await tx.wait();
+
+  const contractBalance = await hre.ethers.provider.getBalance(
+    domainContract.address
+  );
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log(
+    "Contract balance after withdrawal:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+  console.log(
+    "Balance of owner after withdrawal:",
+    hre.ethers.utils.formatEther(ownerBalance)
+  );
 };
 
 const runMain = async () => {
